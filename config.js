f43fca73ab19e22662f96f89d7b76324738e3581
jhernandezl2c-hash <jhernandezl2c@gmail.com>
2026-06-30 16:14:49 -0600

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // Déjalo como cadena vacía "" si todavía no tienes uno.
 
 
-const EMBED_SRC = "https://vileembeds.pages.dev/embed/telemundo-usa" width="100%" height="100%" frameborder="0" scrolling="no" allow="autoplay; encrypted-media; picture-in-picture; fullscreen" allowfullscreen></iframe>";
+const EMBED_SRC = "https://vileembeds.pages.dev/embed/telemundo-usa";
 
 // Nombre del partido o canal que se muestra arriba del reproductor
 const EMBED_LABEL = "Sin transmisión activa";
